b847ae92013afd38f2e6d44c297f4e6fed10862e
tpdk05 <seah0121@kookmin.ac.kr>
2026-09-15 11:36:40 +0900

    Add files via upload

---
+++ 05_practice_2.ino
@@ -0,0 +1,24 @@
+const int LED_PIN = 7;
+
+void setup() {
+  digitalWrite(LED_PIN, HIGH);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_PIN, LOW);
+  delay(1000);
+
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(LED_PIN, HIGH);
+    delay(100);
+
+    digitalWrite(LED_PIN, LOW);
+    delay(100);
+  }
+
+
+  digitalWrite(LED_PIN, HIGH);
+  while (1) {
+  }
+}
